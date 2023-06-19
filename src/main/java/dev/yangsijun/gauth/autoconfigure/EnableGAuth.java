d7aa14396586be89ab5e4b9d1f743cb91c977f64
YangSijun528 <yangsijun5528@gmail.com>
2023-06-19 15:33:47 +0900

    fix EnableGauth

--- src/main/java/dev/yangsijun/gauth/autoconfigure/EnableGAuth.java
+++ src/main/java/dev/yangsijun/gauth/autoconfigure/EnableGAuth.java
@@ -10,6 +10,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Import({GAuthUserService.class})
+@Import({GAuthAutoConfiguration.class})
 public @interface EnableGAuth {
 }
